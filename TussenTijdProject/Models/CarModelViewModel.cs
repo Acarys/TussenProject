bf46e2fe4c19ad43f40b6352355a15aec418eccb
Acarys <jasontoddredhood3@gmail.com>
2024-03-14 15:00:23 +0100

    changes

--- TussenTijdProject/Models/CarModelViewModel.cs
+++ TussenTijdProject/Models/CarModelViewModel.cs
@@ -8,15 +8,15 @@ namespace TussenTijdProject.Client.Models
     public class CarModelViewModel
     {
         public int Category { get; set; }
-        public string Pack { get; set; }
+        public string? Pack { get; set; }
         public Domain.Enums.CarType CarType { get; set; }
         public Brand Brand { get; set; }
         public FuelType FuelType { get; set; }
-        public string Model { get; set; }
+        public string? Model { get; set; }
         public bool IsManual { get; set; }
         public int ExpectedDeliveryPeriodInMonths { get; set; }
-        public string Remarks { get; set; }
-        public TechnicalSpecsItem TechnicalSpecs { get; set; }
-        public FiscalityItem Fiscality { get; set; }
+        public string? Remarks { get; set; }
+        public TechnicalSpecsItem? TechnicalSpecs { get; set; }
+        public FiscalityItem? Fiscality { get; set; }
     }
 }
