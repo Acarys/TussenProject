1a9d1da823e99170d9948d58fa51622caaada405
Robin Proost <robin.proost@inetum-realdolmen.world>
2024-03-14 10:39:19 +0100

    finished domain entities

--- TussenTijdProject.Domain/Sort.cs
+++ TussenTijdProject.Domain/Sort.cs
@@ -3,7 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TussentijdsProject.Domain;
 
 namespace TussenTijdProject.Domain;
-public enum Sort { SUV, Break, Berline, Hatchback, MPV }
+public class Sort
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public ICollection<Brand> Brands { get; set; }
+    public ICollection<CarModel> Cars { get; set; }
+
+}
 
